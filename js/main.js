e86e174c83444c20c9d772eefafe8d878ee6647f
Shevkunova Lera <shevkunova.lera@gmail.com>
2022-05-13 14:23:14 +0300

    adding code

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1175,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                } 
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }  
+            }    
+        ]
+    });
+});
